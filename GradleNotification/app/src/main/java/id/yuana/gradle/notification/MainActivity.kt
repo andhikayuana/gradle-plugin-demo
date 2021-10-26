414c4074a585073d0e1d072da68fbb8f3e3a7496
Andhika Yuana <andhika.yuana@ruangguru.com>
2021-10-26 20:48:22 +0700

    inspect gradle task

---
+++ GradleNotification/app/src/main/java/id/yuana/gradle/notification/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.yuana.gradle.notification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
